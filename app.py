17a41bfbf3c623abb6ac97eb1d2e6543ac22e868
Sambhu Nampoothiri G <67463941+sambhugit@users.noreply.github.com>
2022-05-29 14:17:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 @app.route('/square/<side>')
 def square(side):
     side= int(side)
-    side+=2
-    side-=2
+    side+=3
+    side-=3
     return str(side*side)+"\n"
 
 @app.route('/circle/<radius>')
