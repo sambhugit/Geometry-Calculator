895ebd43b05131c33aea0315be392c776e4c8199
Sambhu Nampoothiri G <67463941+sambhugit@users.noreply.github.com>
2022-05-29 17:39:15 +0530

    Create tobin.py

---
+++ tobin.py
@@ -0,0 +1,8 @@
+import PyInstaller.__main__ 
+import os
+PyInstaller.__main__.run([  
+     'name-%s' % 'hfclpipbinary',
+     '--onefile',
+     '--windowed',
+     os.path.join('/var/lib/jenkins/workspace/hfclpipeline2/', 'app.py'), """your script and path to the script"""                                        
+])
