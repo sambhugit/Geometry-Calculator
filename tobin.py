221f6c6065b4dc8cf4d1d80a698095383ca2db7f
Sambhu Nampoothiri G <67463941+sambhugit@users.noreply.github.com>
2022-05-29 19:16:29 +0530

    Update tobin.py

--- tobin.py
+++ tobin.py
@@ -2,6 +2,5 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([  
      'abcd',
      '--onefile',
-     '--windowed',
-     '/var/lib/jenkins/workspace/hfclpipeline2/app.py'
+     '--windowed'
 ])
