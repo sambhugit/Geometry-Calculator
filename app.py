827f7ac547f44f0b31e8df20a685519db0e8f356
Sambhu Nampoothiri G <67463941+sambhugit@users.noreply.github.com>
2022-05-30 16:37:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,26 @@ def circle(radius):
     radius= int(radius)
     return str(3.14*radius*radius)+"\n"
 
+@app.route('/sphere/<radius>')
+def sphere(radius):
+    radius= int(radius)
+    return str(4*3.14*radius*radius)+"\n"
+
+@app.route('/cuboid/<side>')
+def cuboid(side):
+    side= int(side)
+    return str(6*side*side)+"\n"
+
+@app.route('/spherevol/<radius>')
+def spherevol(radius):
+    radius= int(radius)
+    return str(1.3*3.14*radius*radius*radius)+"\n"
+
+@app.route('/cuboidvol/<side>')
+def cuboidvol(side):
+    side= int(side)
+    return str(side*side*side)+"\n"
+
 @app.route('/')
 def intro():
     return "This is a geometry calculator\n"
